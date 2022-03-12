8bbc9fc0bb71839e18adff2396913e055c9d3f07
JellyTOP <35587996+JellyTOP@users.noreply.github.com>
2022-03-12 23:42:33 +0800

    Update JD_DailyBonus.js

--- JD-DailyBonus/JD_DailyBonus.js
+++ JD-DailyBonus/JD_DailyBonus.js
@@ -46,7 +46,7 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = `[{"cookie": "pt_key=AAJiLLhtADBenxWclGs48qUHI51wtAaWabU_KqvyA_EQ0JZPWTlfrAkMGdO8Y0N3QpUtS02DjvE;pt_pin=15360219298_p;"}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 /*以下样例为双账号("cookie"为必须,其他可选), 第一个账号仅包含Cookie, 第二个账号包含Cookie和金融签到Body: 
 
